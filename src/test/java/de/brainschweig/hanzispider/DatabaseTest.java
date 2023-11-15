52ac6c327866036c376358a53e88ed4ed645b835
Apokalyptus <joern.notbom@gmail.com>
2023-11-15 17:30:34 +0800

    - further refactoring - rudimentary database tests

---
+++ src/test/java/de/brainschweig/hanzispider/DatabaseTest.java
@@ -0,0 +1,38 @@
+package de.brainschweig.hanzispider;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class DatabaseTest {
+
+    Database database;
+
+    @BeforeEach                                         
+    void setUp() {
+        database = new Database("jdbc:mysql://localhost:3306/crawler?user=crawler&password=crawlerX");
+        
+    }
+
+    @Test                                               
+    @DisplayName("Insert Crawl Result")   
+    void testInsertCrawlResult() {
+        database.insertCrawlResult("Ni Hao!");  
+    }
+
+    @Test                             
+    @DisplayName("Insert a list of hyperlinks to Database")
+    void testMultiplyWithZero() {
+        Set<String> stringSet = new HashSet<String>();
+        stringSet.add("Hallo eins");
+        stringSet.add("Hallo zwei");
+        stringSet.add("Hallo drei");
+        stringSet.add("Hallo vier");
+        database.storeHyperLinks(stringSet);
+
+        
+    }
+}
